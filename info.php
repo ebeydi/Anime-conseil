7fbe9a12f788b2cfae1b1cc2cb63e93c3ab325fc
ebeydi <ebeydif@gmail.com>
2025-08-11 06:01:36 +0000

    Delete info.php

--- info.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'root', '', 'essai');
-if ($mysqli->connect_error) {
-    die('Erreur connexion : ' . $mysqli->connect_error);
-}
-echo 'Connexion réussie';
-?>
